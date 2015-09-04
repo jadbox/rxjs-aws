ed77d290b2da282149f461446e600189b8895c33
Jonathan Dunlap <jdunlap@outlook.com>
2015-09-04 16:24:01 -0700

    Env cleanup

--- test.js
+++ test.js
@@ -3,17 +3,12 @@ var rx = require('rx'),
 	rxo = rx.Observable,
 	rxn = rxo.fromNodeCallback,
 	rxarray = rxo.fromArray;
-
 var fs = require('fs');
-var env = require('dotenv').parse(fs.readFileSync(".env"));
 
+var env = require('dotenv').config({silent: true});
 var aws = new (require('./lib/rxjs-aws.js'))();
 
-aws.config.update( 
-	{ 'accessKey': env.AWS_ACCESS_KEY_ID,
-	  'secretKey': env.AWS_SECRET_ACCESS_KEY
-	} );
-
+aws.config.useEnv();
 aws.config.setRegion('us-west-2');
 //console.log(aws.Lambda);
 var lambda = new aws.Lambda();
