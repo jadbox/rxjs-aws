fc355bbda9797d4da335ee579c007e28672d7aef
Jonathan Dunlap <jdunlap@outlook.com>
2015-09-04 16:04:18 -0700

    listFunctionsFlat added, test updated

--- test.js
+++ test.js
@@ -1,5 +1,21 @@
 var rxjs = require('./lib/rxjs-aws.js');
 
-var lambda = new rxjs.Lambda();
+var rx = require('rx'),
+	_ = require('lodash'),
+	rxo = rx.Observable,
+	rxn = rxo.fromNodeCallback,
+	rxarray = rxo.fromArray;
 
-lambda.getFunction('test').subscribe(console.log);
+var fs = require('fs');
+var env = require('dotenv').parse(fs.readFileSync(".env"));
+//console.log(env);
+var aws = new rxjs(
+	{ 'accessKey': env.AWS_ACCESS_KEY_ID,
+	  'secretKey': env.AWS_SECRET_ACCESS_KEY
+	}
+);
+aws.config.setRegion('us-west-2');
+//console.log(aws.Lambda);
+var lambda = new aws.Lambda();
+
+lambda.listFunctionsFlat().pluck('FunctionName').subscribe(console.log);
