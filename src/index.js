2fee112cf6dc7b122d946063f9991e02162f2727
Jonathan Dunlap <jdunlap@outlook.com>
2016-04-24 21:08:54 -0700

    s3 reads working

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-
-const rx = global.Rx || require('rx');
+const rx = global.Rx || require('rxjs/Rx');
 //var rx = require('rxjs-plus');
 var aws = require('aws-sdk');
 var _ = require('lodash');
@@ -7,189 +6,188 @@ var RxNode = require('rx-node');
 
 // Aliases
 var rxo = rx.Observable,
-rxn = rx.Observable.fromNodeCallback,
-rxed = rx.Observable.from;
+  rxn = rx.Observable.bindNodeCallback,
+  rxed = rx.Observable.from;
 
-var ObservableProto = rxo.prototype;
+//var ObservableProto = rxo.prototype;
 
 function wrapAPI(Constr, params, target) {
-	var x = (params instanceof Constr) ? params : new Constr(params);
-	_.forIn(x, function(val, key) {
-	    if (typeof val === 'function') {
-	        //var valBound = val.bind(x);
-	        target[key] = rxn(val, x);
-	        //console.log(key + ");
+  var x = (params instanceof Constr) ? params : new Constr(params);
+  _.forIn(x, function(val, key) {
+    if (typeof val === 'function') {
+      //var valBound = val.bind(x);
+      target[key] = rxn(val.bind(x));
     }
-});
+  });
 
-return x;
+  return x;
 }
 
 // Set Access Key and Secret key from process env
 aws.config.useEnv = function() {
-	aws.config.update({
-	    'accessKey': process.env.AWS_ACCESS_KEY_ID,
-	    'secretKey': process.env.AWS_SECRET_ACCESS_KEY
-	});
-	return aws.config;
+  aws.config.update({
+    'accessKey': process.env.AWS_ACCESS_KEY_ID,
+    'secretKey': process.env.AWS_SECRET_ACCESS_KEY,
+    'secretKey': process.env.AWS_REGION
+  });
+  return aws.config;
 };
 
 // Shorthand for region change
 aws.config.setRegion = function(region) {
-	aws.config.update({
-	    region: region
-	});
-	return aws.config;
+  aws.config.update({
+    region: region
+  });
+  return aws.config;
 };
 
-rxo.aws =
-rxo.prototype.aws = {
+export default function AWS(options) {
+	options = options || {};
+
+  return {
     '_': aws,
     'config': aws.config,
-    'debug': false,
     'S3': function(params) {
-        var wrapper = {};
-        this._s3 = wrapAPI(aws.S3, params, wrapper);
-        var _s3 = this._s3;
+      var wrapper = {};
+      this._s3 = wrapAPI(aws.S3, params, wrapper);
+      var _s3 = this._s3;
 
-        var _listBuckets = wrapper.listBuckets;
+      wrapper.listBucketNames = function listBucketNamesMethod() {
+				var _listBuckets = wrapper.listBuckets;
+        return _listBuckets().pluck('Buckets').flatMap(rxed).pluck('Name');
+      };
 
-        wrapper.listBucketNames = function listBucketNamesMethod() {
-            return _listBuckets().pluck('Buckets').flatMap(rxo.fromArray).pluck('Name');
+      wrapper.listObjects$ = function(params) {
+        if (!params.marker) {
+          //params.marker = 0;
+        }
+        return rxo.defer(() => wrapper.listObjects(params))
+          .flatMap(x => {
+            const justx = rxo.of(x);
+            if (x.IsTruncated && x.NextMarker) {
+              params.Marker = x.NextMarker;
+              return justx.merge(wrapper.listObjects$(params));
+            } else if (x.IsTruncated && !x.NextMarker) {
+              const length = x.Contents.length;
+              const Key = x.Contents[length - 1].Key;
+              params.Marker = Key;
+              return justx.merge(wrapper.listObjects$(params));
+            } else return justx;
+          })
+      }
+
+      wrapper.deleteObject$ = function(url) {
+        if (url.indexOf('/') === -1) throw new Error('missing bucket name');
+        var bucket = url.split('/')[0];
+        url = url.replace(bucket + '/', '');
+        if (options.debug) console.log("Delete from s3", "bucket: " + bucket, "key: " + url);
+        var obj = {
+          Bucket: bucket,
+          Key: url
         };
+        return wrapper.deleteObject(obj);
+      };
 
-        wrapper.listObjects$ = function(params) {
-        	if(!params.marker) {
-        		//params.marker = 0;
-        	}
-            return rxo.defer(() => wrapper.listObjects(params))
-            		.flatMap( x => {
-            			const justx = rxo.just(x);
-            			if(x.IsTruncated && x.NextMarker) {
-            				params.Marker = x.NextMarker;
-							return justx.merge( wrapper.listObjects$(params) );
-            			}
-            			else if(x.IsTruncated && !x.NextMarker) {
-            				const length = x.Contents.length;
-            				const Key = x.Contents[length-1].Key;
-            				params.Marker = Key;
-							return justx.merge( wrapper.listObjects$(params) );
-            			}
-            			else return justx;
-            		})
+      wrapper.getObjectStream = function getObjectAsStream(url, range, pipe) {
+        if (url.indexOf('/') === -1) throw new Error('missing bucket name');
+        var bucket = url.split('/')[0];
+        url = url.replace(bucket + '/', '');
+
+        var obj = {
+          Bucket: bucket,
+          Key: url
+        };
+        if (range) {
+          if (range.length < 2 || range[1] === -1) obj.Range = 'bytes=' + range[0] + '-';
+          else obj.Range = 'bytes=' + range[0] + '-' + range[1];
+        }
+        if (options.debug) console.log("Streaming from s3", "bucket: " + bucket, "key: " + url);
+
+        var stream = pipe ? _s3.getObject(obj).createReadStream().pipe(pipe) :
+          _s3.getObject(obj).createReadStream();
+
+        return RxNode.fromReadableStream(stream)
+          //.pluck('Body')
+      };
+
+      wrapper.getObjectAsString = function getObjectAsStringMethod(url) {
+        if (url.indexOf('/') === -1) throw new Error('missing bucket name');
+        var bucket = url.split('/')[0];
+        url = url.replace(bucket + '/', '');
+        var milliseconds;
+
+        var obj = {
+          Bucket: bucket,
+          Key: url
+        };
+        return rxo.defer(function() {
+            milliseconds = (new Date).getTime();
+            return wrapper.getObject(obj)
+          })
+          //.retry(3)
+          .do(function(x) {
+            var finised_milliseconds = (new Date).getTime() - milliseconds;
+            if (options.debug) console.log("Fetched from s3", "bucket: " + bucket, "key: " + url + ' time: ' + finised_milliseconds);
+          })
+          .pluck('Body')
+          .map(x => x.toString());
+      };
+
+      // Todo: defer here as well?
+      wrapper.putObjectAsString = function(url, acl) {
+        return function(data) {
+          var bucket = url.split('/')[0];
+          url = url.replace(bucket + '/', '');
+          var obj = {
+            Bucket: bucket,
+            Key: url
+          };
+          obj.Body = data;
+          if (acl) obj.ACL = acl;
+          var milliseconds = (new Date).getTime();
+          return wrapper.putObject(obj)
+            .do(function(x) {
+              var finised_milliseconds = (new Date).getTime() - milliseconds;
+              if (options.debug) console.log("Saved to s3", "bucket: " + bucket, "key: " + url + ' time: ' + finised_milliseconds);
+            });
         }
 
-        wrapper.deleteObject$ = function(url) {
-            if(url.indexOf('/') === -1) throw new Error('missing bucket name');
-            var bucket = url.split('/')[0];
-            url = url.replace(bucket + '/', '');
-            if(rxo.aws.debug) console.log("Delete from s3", "bucket: " + bucket, "key: " + url);
-            var obj = {
-                Bucket: bucket,
-                Key: url
-            };
-            return wrapper.deleteObject(obj);
-        };
+      };
 
-        wrapper.getObjectStream = function getObjectAsStream(url, range, pipe) {
-            if(url.indexOf('/') === -1) throw new Error('missing bucket name');
-            var bucket = url.split('/')[0];
-            url = url.replace(bucket + '/', '');
-            
-            var obj = {
-                Bucket: bucket,
-                Key: url
-            };
-            if(range) {
-                if(range.length < 2 || range[1] === -1) obj.Range = 'bytes='+range[0]+'-';
-                else obj.Range = 'bytes='+range[0]+'-'+range[1];
-            }
-            if(rxo.aws.debug) console.log("Streaming from s3", "bucket: " + bucket, "key: " + url);
-            
-            var stream =  pipe ? _s3.getObject(obj).createReadStream().pipe(pipe) :
-                        _s3.getObject(obj).createReadStream();
-                        
-            return RxNode.fromReadableStream(stream)
-                //.pluck('Body')
-        };
-
-        wrapper.getObjectAsString = function getObjectAsStringMethod(url) {
-            if(url.indexOf('/') === -1) throw new Error('missing bucket name');
-            var bucket = url.split('/')[0];
-            url = url.replace(bucket + '/', '');
-            var milliseconds;
-            
-            var obj = {
-                Bucket: bucket,
-                Key: url
-            };
-            return rxo.defer(function() {
-                    milliseconds = (new Date).getTime();
-                    return wrapper.getObject(obj)
-                })
-                //.retry(3)
-                .do(function(x) {
-                    var finised_milliseconds = (new Date).getTime() - milliseconds;
-                    if(rxo.aws.debug) console.log("Fetched from s3", "bucket: " + bucket, "key: " + url + ' time: ' + finised_milliseconds);
-                })
-                .pluck('Body')
-                .map(x=>x.toString());
-        };
-
-        // Todo: defer here as well?
-        ObservableProto.putObjectAsString =
-            wrapper.putObjectAsString = function(url, acl) {
-                return function(data) {
-                    var bucket = url.split('/')[0];
-                    url = url.replace(bucket + '/', '');
-                    var obj = {
-                        Bucket: bucket,
-                        Key: url
-                    };
-                    obj.Body = data;
-                    if (acl) obj.ACL = acl;
-                    var milliseconds = (new Date).getTime();
-                    return wrapper.putObject(obj)
-                        .do(function(x) {
-                            var finised_milliseconds = (new Date).getTime() - milliseconds;
-                            if(rxo.aws.debug) console.log("Saved to s3", "bucket: " + bucket, "key: " + url + ' time: ' + finised_milliseconds);
-                        });
-                }
-
-            };
-
-        return wrapper;
+      return wrapper;
     },
     'Lambda': function(params) {
-        var wrapper = {};
-        var _lambda =
-            this._lambda = wrapAPI(aws.Lambda, params, wrapper);
+      var wrapper = {};
+      var _lambda =
+        this._lambda = wrapAPI(aws.Lambda, params, wrapper);
 
-        var _listFunctions = wrapper.listFunctions;
-        wrapper.listFunctions = function() {
-            return _listFunctions().pluck('Functions').flatMap(rxo.fromArray);
-        }
+      var _listFunctions = wrapper.listFunctions;
+      wrapper.listFunctions = function() {
+        return _listFunctions().pluck('Functions').flatMap(rxed);
+      }
 
-        return wrapper;
+      return wrapper;
     },
     'DynamoDB': function(params) {
-        var wrapper = {};
-        this._dynamodb = wrapAPI(aws.DynamoDB, params, wrapper);
+      var wrapper = {};
+      this._dynamodb = wrapAPI(aws.DynamoDB, params, wrapper);
 
-        return wrapper;
+      return wrapper;
     },
     'SNS': function(params) {
-        var wrapper = {};
-        this._sns = wrapAPI(aws.SNS, params, wrapper);
+      var wrapper = {};
+      this._sns = wrapAPI(aws.SNS, params, wrapper);
 
-        return wrapper;
+      return wrapper;
     },
     'SQS': function(params) {
-        var wrapper = {};
-        this._sqs = wrapAPI(aws.SQS, params, wrapper);
+      var wrapper = {};
+      this._sqs = wrapAPI(aws.SQS, params, wrapper);
 
-        return wrapper;
+      return wrapper;
     }
+  }
+
 };
 
-exports = module.exports = rx;
+//exports = module.exports = rx;
--- test.js
+++ test.js
@@ -1,22 +1,30 @@
-var rx = require('rxjs-plus'), 
+var rx = require('rxjs/Rx'),
+	AWS = require('./dist/rxjs-aws.js')
 	rxo = rx.Observable,
-	_ = require('lodash-fp'),
-	rxn = rx.Observable.fromNodeCallback;
+	_ = require('lodash');
 var fs = require('fs');
 //console.log("global.Rx", global.Rx);
 var env = require('dotenv').config({silent: true});
 
-require('./lib/rxjs-aws.js');
+//require('./lib/rxjs-aws.js');
 
 //console.log("rxo.aws", rxo.aws);
-var aws = rxo.aws;
 
-aws.config.useEnv().setRegion('us-west-2');
+//aws.config.useEnv().setRegion('us-west-2');
 //console.log(aws.Lambda);
 //console.log("rxo aws", rxo.aws);
-var lambda = new aws.Lambda();
+var aws = new AWS.default();
+var s3 = new aws.S3();
+s3.listBucketNames().subscribe(function(x){
+	console.log(x);
+	s3.getObjectAsString('vast-server-tracking-west-2/v1/2016/02/26/01/vast-server-2-2016-02-26-01-13-02-062d4886-0342-40dc-ad01-907e4cf8bd2c')
+	.subscribe(x=>{
+		console.log('s3', x);
+	})
+})
+//var lambda = new aws.Lambda();
 
 //var x = _.filter({'name': 'joe'}, [{'name':'joe', active:false}, {'name':'bob', active:false}]);
 //console.log(x);
 //lambda.listFunctions.pluck('FunctionName').filterIncludes('Pi').subscribe(console.log);
-lambda.listFunctions().propertyIncludes('FunctionName', 'Lambda').subscribe(console.log);
+//lambda.listFunctions().propertyIncludes('FunctionName', 'Lambda').subscribe(console.log);
